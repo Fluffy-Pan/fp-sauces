cb34ffb4b2af3d0962d8352e0361558a01ef36e3
Weng Long Pang (ECHO) <its@thevalentine.flowers>
2021-10-07 00:00:40 +0800

    ECHO initial commit

---
+++ sauce.cmd
@@ -0,0 +1,30 @@
+@echo off
+:: Prepare timestamp
+set zerotime=%time: =0%
+set outputtime=%date:~0,4%-%date:~5,2%-%date:~8,2%T%zerotime:~0,8%+08:00
+
+echo Checking internet connection...
+ping %1 -n 1 -w 1000 > NUL
+
+if errorlevel 1 (GOTO FALLBACK) else (GOTO ALLGOOD)
+
+:FALLBACK
+echo Internet Test Failed
+:: YOUR CODE
+jq -c ". += [{\"time\":\"%outputtime%\",\"status\":\"NOTOK\"}]" %2 >temp.json
+::...
+GOTO FINISH
+
+:ALLGOOD
+echo Internet Test was successful
+:: MAYBE REVERSE THE METRICS?
+jq -c ". += [{\"time\":\"%outputtime%\",\"status\":\"OK\"}]" %2 >temp.json
+GOTO FINISH
+
+:FINISH
+xcopy /Y temp.json %2
+git pull origin %3
+git add %2
+git commit -m"%2 Updated"
+git push origin %3
+exit 0
